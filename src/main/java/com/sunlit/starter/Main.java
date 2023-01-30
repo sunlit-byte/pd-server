9a7d8d5689c1b55bdd9836ea9fea475730308438
Sunlit <1265393648@qq.com>
2023-01-30 21:04:37 +0800

    just begin

---
+++ src/main/java/com/sunlit/starter/Main.java
@@ -0,0 +1,18 @@
+package com.sunlit.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author YourName
+ * @date 2023-01-29 23:33
+ **/
+
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class);
+    }
+
+}
